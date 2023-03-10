1e0eb9309fbdb2334185785776fd5981886a2801
jozkan30 <jozkan2@gmail.com>
2023-03-09 21:06:11 -0500

    update

---
+++ src/components/Filters.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './filters.css'
+
+export default function Filters(props) {
+  return (
+    <div className='filters'>
+      <button className='buttons'> {props.category}</button>
+    </div>
+  )
+}
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,59 +1,76 @@
-import"./home.css"
+import "./home.css";
 import ItemCard from "../components/ItemCard.jsx";
-import { useEffect, useState} from "react";
-
-import React from 'react'
+import { useEffect, useState } from "react";
+import Filters from "../components/Filters";
+import React from "react";
 
 export default function Home() {
-    const [data, setData] = useState([]);
-    const [item,  setItems] = useState("")
+  const [data, setData] = useState([]);
+  const [item, setItems] = useState("");
 
-    useEffect(() => {
-      async function fetchData() {
-        const response = await fetch("https://fake-store-api-production-c25d.up.railway.app/items");
-        const data = await response.json();
-        if( item === "most expensive" ){
-         setData(data.sort((a,b)=> b.price - a.price))
-        } else if (item === "least expensive"){
-        setData(data.sort((a,b)=> a.price - b.price))
-        } else{
-          setData(data)
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://fake-store-api-production-c25d.up.railway.app/items"
+      );
+      const data = await response.json();
+      if (item === "most expensive") {
+        setData(data.sort((a, b) => b.price - a.price));
+      } else if (item === "least expensive") {
+        setData(data.sort((a, b) => a.price - b.price));
+      } else {
+        setData(data);
       }
-      fetchData();
-    }, [item]);
-  
-  
-// Filter Buttons
-let filterByMost = () =>{
-setItems("most expensive")
-}
-let filterByLeast = () =>{
-setItems("least expensive")
-}
-let clearFilter = () =>{
-setItems("clear")
-}
+    }
+    fetchData();
+  }, [item]);
 
-return (
+  const categories = Array.from(new Set(data.map((item) => item.category)));
 
+  console.log(categories);
+  // Sort Buttons
+  let filterByMost = () => {
+    setItems("most expensive");
+  };
+  let filterByLeast = () => {
+    setItems("least expensive");
+  };
+  let clearFilter = () => {
+    setItems("clear");
+  };
+
+  return (
     <div className="main container">
-        <div>
-            <button className="filterBox" onClick={filterByMost}> Most Expensive </button>
-            <button className="filterBox" onClick={filterByLeast}> Least Expensive </button>
-            <button className="filterBox" onClick={clearFilter}> Clear Filters </button>
+        <div className="cats">
+          {categories.map((category, index) => (
+            <Filters category={category} />
+          ))}
+        
         </div>
-        <div className="item-listings">
-        {data.map((item)=>(
-            <ItemCard 
+        <div>
+        <button className="filterBox" onClick={filterByMost}>
+          {" "}
+          Most Expensive{" "}
+        </button>
+        <button className="filterBox" onClick={filterByLeast}>
+          {" "}
+          Least Expensive{" "}
+        </button>
+        <button className="filterBox" onClick={clearFilter}>
+          {" "}
+          Clear Filters{" "}
+        </button>
+      </div>
+      <div className="item-listings">
+        {data.map((item) => (
+          <ItemCard
             img={item.image}
             id={item._id}
-            title ={item.title}
-            price ={ `From $${item.price}`}
-            />
+            title={item.title}
+            price={`From $${item.price}`}
+          />
         ))}
-        </div>
-      
+      </div>
     </div>
-  )
+  );
 }
